bbb15d4ab4db3bf23eb574f874400bba3a9f54f2
dadeferra2001 <f.davide2001@gmail.com>
2024-09-25 14:49:27 +0200

    Modified trainers for OLID

--- gpt2_trainer.py
+++ gpt2_trainer.py
@@ -3,38 +3,38 @@ from datasets import  Dataset
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
 
 
-for i in range(1,4):
+# for i in range(1,4):
 
-    # Initializing the model and the tokenizer
-    print("Loading model..")
-    tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2")
-    tokenizer.pad_token = tokenizer.eos_token
-    model = AutoModelForSequenceClassification.from_pretrained("openai-community/gpt2", num_labels=3)
-    model.config.pad_token_id = model.config.eos_token_id
+# Initializing the model and the tokenizer
+print("Loading model..")
+tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2")
+tokenizer.pad_token = tokenizer.eos_token
+model = AutoModelForSequenceClassification.from_pretrained("openai-community/gpt2", num_labels=2)
+model.config.pad_token_id = model.config.eos_token_id
 
-    # Reading the data
-    print("Loading data..")
-    df_train = pd.read_csv(f"datasets/dataset{i}_train.csv")
-    train_dataset = Dataset.from_pandas(df_train, preserve_index = False)
-    tokenized_train_dataset = train_dataset.map(lambda x: tokenizer(x["text"], padding="max_length", max_length=128, truncation=True), batched=True)
+# Reading the data
+print("Loading data..")
+df_train = pd.read_csv(f"datasets/dataset4_train.csv")
+train_dataset = Dataset.from_pandas(df_train, preserve_index = False)
+tokenized_train_dataset = train_dataset.map(lambda x: tokenizer(x["text"], padding="max_length", max_length=128, truncation=True), batched=True)
 
-    # Creating HF Trainer
-    training_args = TrainingArguments(
-        output_dir=f"savings/gpt2-savings-dataset{i}", 
-        evaluation_strategy="no",
-        per_device_train_batch_size=32)
+# Creating HF Trainer
+training_args = TrainingArguments(
+    output_dir=f"savings/gpt2-savings-dataset4", 
+    evaluation_strategy="no",
+    per_device_train_batch_size=32)
 
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=tokenized_train_dataset
-    )
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_train_dataset
+)
 
-    # Train the model
-    print("Training..")
-    trainer.train()
+# Train the model
+print("Training..")
+trainer.train()
 
-    # Save the model and the tokenizer
-    print("Saving model")
-    trainer.save_model(f'savings/models/gpt2-dataset{i}')
-    tokenizer.save_pretrained(f'savings/models/gpt2-dataset{i}')
+# Save the model and the tokenizer
+print("Saving model")
+trainer.save_model(f'savings/models/gpt2-dataset4')
+tokenizer.save_pretrained(f'savings/models/gpt2-dataset4')
--- xlnet_trainer.py
+++ xlnet_trainer.py
@@ -3,38 +3,38 @@ from datasets import  Dataset
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
 
 
-for i in range(1,4):
+# for i in range(1,4):
 
-    # Initializing the model and the tokenizer
-    print("Loading model..")
-    tokenizer = AutoTokenizer.from_pretrained("xlnet/xlnet-base-cased")
-    model = AutoModelForSequenceClassification.from_pretrained("xlnet/xlnet-base-cased", num_labels=3)
+# Initializing the model and the tokenizer
+print("Loading model..")
+tokenizer = AutoTokenizer.from_pretrained("xlnet/xlnet-base-cased")
+model = AutoModelForSequenceClassification.from_pretrained("xlnet/xlnet-base-cased", num_labels=2)
 
-    # Reading the data
-    print("Loading data..")
-    df_train = pd.read_csv(f"datasets/dataset{i}_train.csv")
-    train_dataset = Dataset.from_pandas(df_train, preserve_index = False)
-    tokenized_train_dataset = train_dataset.map(lambda x: tokenizer(x["text"], padding="max_length", max_length=128, truncation=True), batched=True)
+# Reading the data
+print("Loading data..")
+df_train = pd.read_csv(f"datasets/datasetdd4_train.csv")
+train_dataset = Dataset.from_pandas(df_train, preserve_index = False)
+tokenized_train_dataset = train_dataset.map(lambda x: tokenizer(x["text"], padding="max_length", max_length=128, truncation=True), batched=True)
 
-    # Creating HF Trainer
-    training_args = TrainingArguments(
-        output_dir=f"savings/xlnet-savings-dataset{i}", 
-        evaluation_strategy="no",
-        per_device_train_batch_size=32)
+# Creating HF Trainer
+training_args = TrainingArguments(
+    output_dir=f"savings/xlnet-savings-datasetdd4", 
+    evaluation_strategy="no",
+    per_device_train_batch_size=32)
 
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=tokenized_train_dataset
-    )
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_train_dataset
+)
 
-    # Train the model
-    print("Training..")
-    trainer.train()
+# Train the model
+print("Training..")
+trainer.train()
 
-    # Save the model and the tokenizer
-    print("Saving model")
-    trainer.save_model(f'savings/models/xlnet-dataset{i}')
-    tokenizer.save_pretrained(f'savings/models/xlnet-dataset{i}')
-    
+# Save the model and the tokenizer
+print("Saving model")
+trainer.save_model(f'savings/models/xlnet-datasetdd4')
+tokenizer.save_pretrained(f'savings/models/xlnet-datasetdd4')
+
 
